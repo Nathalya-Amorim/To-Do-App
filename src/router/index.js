4b555e655bbd1da503945b18d84a76edbdcb8d80
Nathalya Amorim <nathalyamorim87@gmail.com>
2022-11-22 13:31:40 -0500

    adding functionalities

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/ToDo.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'ToDo',
     component: HomeView
   },
   {
